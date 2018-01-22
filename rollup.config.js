0e10fa12e65b78a08adde601b5157c292d1c8dfa
A Jordan Simonds <jsimonds@onshape.com>
2018-01-22 16:03:49 -0500

    Improve rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,40 +1,25 @@
 import babel from 'rollup-plugin-babel';
 import uglify from 'rollup-plugin-uglify';
+import pkg from './package.json';
 
 const input =  './src/index.js';
-const fileName = 'vue-slicksort';
 const moduleName = 'VueSlicksort';
 
 export default [
   {
     input,
-    output: {
-      file: './dist/commonjs/index.js',
-      format: 'cjs',
-    },
+    output: [
+      { file: pkg.main, format: 'cjs' },
+      { file: pkg.module, format: 'es' },
+      { file: pkg.browser, format: 'umd', name: moduleName },
+    ],
     plugins: [ 
       babel(),
     ],
   }, {
     input,
     output: {
-      file: './dist/es6/index.js',
-      format: 'es',
-    },
-  }, {
-    input,
-    output: {
-      file: './dist/umd/' + fileName + '.js',
-      name: moduleName,
-      format: 'umd',
-    },
-    plugins: [ 
-      babel(),
-    ],
-  }, {
-    input,
-    output: {
-      file: './dist/umd/' + fileName + '.min.js',
+      file: pkg.unpkg,
       name: moduleName,
       format: 'umd',
       sourcemap: true,
