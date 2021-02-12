ce12398cca575b7631833bbcee5670d01d31a8a4
Dave Stewart <dave@davestewart.co.uk>
2021-02-12 18:26:35 +0000

    Deduplicate code with factory

--- src/components.js
+++ src/components.js
@@ -1,25 +1,21 @@
 import { ElementMixin } from './ElementMixin';
 import { ContainerMixin } from './ContainerMixin';
 
-export const SlickList = {
-  name: 'slick-list',
-  mixins: [ ContainerMixin ],
-  props: {
-    tag: {
-      type: String,
-      default: 'div',
+function create (name, mixin) {
+  return {
+    name,
+    mixins: [ mixin ],
+    props: {
+      tag: {
+        type: String,
+        default: 'div',
+      },
     },
-  },
-  render (h) {
-    return h(this.tag, this.$slots.default);
-  },
-};
+    render (h) {
+      return h(this.tag, this.$slots.default);
+    },
+  };
+}
 
-export const SlickItem = {
-  name: 'slick-item',
-  mixins: [ ElementMixin ],
-  props: [ 'tag' ],
-  render (h) {
-    return h(this.tag, this.$slots.default);
-  },
-};
+export const SlickList = create('slick-list', ContainerMixin);
+export const SlickItem = create('slick-item', ElementMixin);
