2b088a11272851fd81cd636fc22ed9179c86ac65
A Jordan Simonds <simonds.jordan@gmail.com>
2021-02-15 21:01:16 -0500

    feat: add drag handle to plugin

--- src/plugin.js
+++ src/plugin.js
@@ -1,7 +1,9 @@
 import SlicksortHub from './SlicksortHub';
+import { HandleDirective } from './HandleDirective';
 
 export default {
   install(app, options) {
+    app.directive('drag-handle', HandleDirective);
     app.provide('SlicksortHub', new SlicksortHub(options));
   },
 };
